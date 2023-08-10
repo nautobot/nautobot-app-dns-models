91267dcfdf737926d9fbef46dd1dd083af8fc445
scetron <stephen.corry@networktocode.com>
2023-08-10 09:54:41 -0400

    add complete api urls and views

--- nautobot_dns_models/api/urls.py
+++ nautobot_dns_models/api/urls.py
@@ -7,7 +7,13 @@ from nautobot_dns_models.api import views
 router = OrderedDefaultRouter()
 # add the name of your api endpoint, usually hyphenated model name in plural, e.g. "my-model-classes"
 router.register("dnszonemodel", views.DnsZoneModelViewSet)
-
+# router.register("nsrecordmodel", views.NSRecordModelViewSet)
+router.register("arecordmodel", views.ARecordModelViewSet)
+router.register("aaaarecordmodel", views.AAAARecordModelViewSet)
+router.register("cnamerecordmodel", views.CNameRecordModelViewSet)
+router.register("mxrecordmodel", views.MXRecordModelViewSet)
+router.register("txtrecordmodel", views.TXTRecordModelViewSet)
+                
 
 app_name = "nautobot_dns_models-api"
 urlpatterns = router.urls
--- nautobot_dns_models/api/views.py
+++ nautobot_dns_models/api/views.py
@@ -14,5 +14,49 @@ class DnsZoneModelViewSet(ModelViewSet):  # pylint: disable=too-many-ancestors
     serializer_class = serializers.DnsZoneModelSerializer
     filterset_class = filters.DnsZoneModelFilterSet
 
+    lookup_field = "pk"
     # Option for modifying the default HTTP methods:
     # http_method_names = ["get", "post", "put", "patch", "delete", "head", "options", "trace"]
+
+class ARecordModelViewSet(ModelViewSet):
+    """"""
+    queryset = models.ARecordModel.objects.all()
+    serializer_class = serializers.ARecordModelSerializer
+    filterset_class = filters.ARecordModelFilterSet
+
+    lookup_field = "pk"
+
+
+class AAAARecordModelViewSet(ModelViewSet):
+    """"""
+    
+    queryset = models.AAAARecordModel.objects.all()
+    serializer_class = serializers.AAAARecordModelSerializer
+    filterset_class = filters.AAAARecordModelFilterSet
+
+    lookup_field = "pk"
+
+class CNameRecordModelViewSet(ModelViewSet):
+
+    queryset = models.CNAMERecordModel.objects.all()
+    serializer_class = serializers.CNAMERecordModelSerializer
+    filterset_class = filters.CNAMERecordModelFilterSet
+
+    lookup_field = "pk"
+
+class MXRecordModelViewSet(ModelViewSet):
+
+    queryset = models.MXRecordModel.objects.all()
+    serializer_class = serializers.MXRecordModelSerializer
+    filterset_class = filters.MXRecordModelFilterSet
+
+    lookup_field = "pk"
+
+
+class TXTRecordModelViewSet(ModelViewSet):
+
+    queryset = models.TXTRecordModel.objects.all()
+    serializer_class = serializers.TXTRecordModelSerializer
+    filterset_class = filters.TXTRecordModelFilterSet
+
+    lookup_field = "pk"
