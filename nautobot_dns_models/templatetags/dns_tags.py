1fa6b51daa0848e8bbbb8b566e765499ead13d67
Gerasimos Tzakis <gerasimos.tzakis@networktocode.com>
2024-04-18 16:32:00 +0300

    fix permissions issue in template

--- nautobot_dns_models/templatetags/dns_tags.py
+++ nautobot_dns_models/templatetags/dns_tags.py
@@ -23,8 +23,42 @@ def url_with_action(value, action):
         action (str): Necessary action (eg. edit)
 
     Returns:
-        _type_: String URL of the specific object's action
+        str: String URL of the specific object's action
     """
     if value is None:
         return HTML_NONE
     return reverse(lookup.get_route_for_model(value, action), args=[value.pk])
+
+
+@library.filter()
+@register.filter()
+def user_has_change_access(user, instance):
+    """Return URL of a model based on the action.
+
+    Args:
+        user (obj): Object of a User
+        instance (obj): Instance of a Model
+
+    Returns:
+        bool: Returns true/false based on user's change permissions.
+    """
+    if user is None:
+        return HTML_NONE
+    return user.has_perm(f"nautobot_dns_models.change_{instance._meta.model_name}")
+
+
+@library.filter()
+@register.filter()
+def user_has_delete_access(user, instance):
+    """Return URL of a model based on the action.
+
+    Args:
+        user (obj): Object of a User
+        instance (obj): a Model instance
+
+    Returns:
+        bool: Returns true/false based on user's delete permissions.
+    """
+    if user is None:
+        return HTML_NONE
+    return user.has_perm(f"nautobot_dns_models.delete_{instance._meta.model_name}")
