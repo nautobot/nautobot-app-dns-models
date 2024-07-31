4e17fb7ff32707584de63d0a7b6b16f9be633114
scetron <stephen.corry@networktocode.com>
2024-07-31 18:29:22 -0400

    update urls

--- nautobot_dns_models/api/urls.py
+++ nautobot_dns_models/api/urls.py
@@ -6,14 +6,14 @@ from nautobot_dns_models.api import views
 
 router = OrderedDefaultRouter()
 # add the name of your api endpoint, usually hyphenated model name in plural, e.g. "my-model-classes"
-router.register("dnszonemodel", views.DNSZoneModelViewSet)
-router.register("nsrecordmodel", views.NSRecordModelViewSet)
-router.register("arecordmodel", views.ARecordModelViewSet)
-router.register("aaaarecordmodel", views.AAAARecordModelViewSet)
-router.register("cnamerecordmodel", views.CNameRecordModelViewSet)
-router.register("mxrecordmodel", views.MXRecordModelViewSet)
-router.register("txtrecordmodel", views.TXTRecordModelViewSet)
-router.register("ptrrecordmodel", views.PTRRecordModelViewSet)
+router.register("dns-zone-records", views.DNSZoneModelViewSet)
+router.register("ns-records", views.NSRecordModelViewSet)
+router.register("a-records", views.ARecordModelViewSet)
+router.register("aaaa-records", views.AAAARecordModelViewSet)
+router.register("cname-records", views.CNameRecordModelViewSet)
+router.register("mx-records", views.MXRecordModelViewSet)
+router.register("txt-records", views.TXTRecordModelViewSet)
+router.register("ptr-records", views.PTRRecordModelViewSet)
 
 app_name = "nautobot_dns_models-api"
 urlpatterns = router.urls
--- nautobot_dns_models/urls.py
+++ nautobot_dns_models/urls.py
@@ -10,52 +10,15 @@ from nautobot_dns_models import views
 
 router = NautobotUIViewSetRouter()
 
-router.register("dnszonemodel", views.DNSZoneModelViewSet)
-router.register("nsrecordmodel", views.NSRecordModelViewSet)
-router.register("arecordmodel", views.ARecordModelViewSet)
-router.register("aaaarecordmodel", views.AAAARecordModelViewSet)
-router.register("cnamerecordmodel", views.CNAMERecordModelViewSet)
-router.register("mxrecordmodel", views.MXRecordModelViewSet)
-router.register("txtrecordmodel", views.TXTRecordModelViewSet)
-router.register("ptrrecordmodel", views.PTRRecordModelViewSet)
+router.register("dnszone", views.DNSZoneModelViewSet)
+router.register("nsrecord", views.NSRecordModelViewSet)
+router.register("arecord", views.ARecordModelViewSet)
+router.register("aaaarecord", views.AAAARecordModelViewSet)
+router.register("cnamerecord", views.CNAMERecordModelViewSet)
+router.register("mxrecord", views.MXRecordModelViewSet)
+router.register("txtrecord", views.TXTRecordModelViewSet)
+router.register("ptrrecord", views.PTRRecordModelViewSet)
 
-urlpatterns = [
-    # path(
-    #     "dnszonemodel/<uuid:pk>/changelog/",
-    #     ObjectChangeLogView.as_view(),
-    #     name="dnszonemodel_changelog",
-    #     kwargs={"model": models.DnsZoneModel},
-    # ),
-    # path(
-    #     "dnszonemodel/<slug:slug>/changelog/",
-    #     ObjectChangeLogView.as_view(),
-    #     name="dnszonemodel_changelog",
-    #     kwargs={"model": models.DnsZoneModel},
-    # ),
-    # path(
-    #     "dnszonemodel/<uuid:pk>/notes/",
-    #     ObjectNotesView.as_view(),
-    #     name="dnszonemodel_notes",
-    #     kwargs={"model": models.DnsZoneModel},
-    # ),
-]
+urlpatterns = []
 
 urlpatterns += router.urls
-
-# urlpatterns = [
-#     # DnsZoneModel URLs
-#     path("dnszonemodel/", dnszonemodel.DnsZoneModelListView.as_view(), name="dnszonemodel_list"),
-#     # Order is important for these URLs to work (add/delete/edit) to be before any that require uuid/slug
-#     path("dnszonemodel/add/", dnszonemodel.DnsZoneModelCreateView.as_view(), name="dnszonemodel_add"),
-#     path("dnszonemodel/delete/", dnszonemodel.DnsZoneModelBulkDeleteView.as_view(), name="dnszonemodel_bulk_delete"),
-#     path("dnszonemodel/edit/", dnszonemodel.DnsZoneModelBulkEditView.as_view(), name="dnszonemodel_bulk_edit"),
-#     path("dnszonemodel/<slug:slug>/", dnszonemodel.DnsZoneModelView.as_view(), name="dnszonemodel"),
-#     path("dnszonemodel/<slug:slug>/delete/", dnszonemodel.DnsZoneModelDeleteView.as_view(), name="dnszonemodel_delete"),
-#     path("dnszonemodel/<slug:slug>/edit/", dnszonemodel.DnsZoneModelEditView.as_view(), name="dnszonemodel_edit"),
-#     path(
-#         "dnszonemodel/<slug:slug>/changelog/",
-#         ObjectChangeLogView.as_view(),
-#         name="dnszonemodel_changelog",
-#         kwargs={"model": models.DnsZoneModel},
-#     ),
-# ]
