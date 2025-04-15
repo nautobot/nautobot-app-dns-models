bc1193f4e782a424c327403de9748a75bf299fde
Nikos Kallergis <nikos.kallergis@networktocode.com>
2025-04-15 18:08:53 +0300

    Use the UI Component Framework for PTR panels too

--- nautobot_dns_models/template_content.py
+++ nautobot_dns_models/template_content.py
@@ -5,7 +5,11 @@ from nautobot.core.views.utils import get_obj_from_context
 from netutils.ip import ipaddress_address
 
 from nautobot_dns_models.models import PTRRecordModel
-from nautobot_dns_models.tables import AAAARecordModelTable, ARecordModelTable
+from nautobot_dns_models.tables import (
+    AAAARecordModelTable,
+    ARecordModelTable,
+    PTRRecordModelTable,
+)
 
 
 class ForwardDNSRecordsTablePanel(ObjectsTablePanel):
@@ -13,12 +17,37 @@ class ForwardDNSRecordsTablePanel(ObjectsTablePanel):
 
     def get_extra_context(self, context):
         """Set the table class based on the IP version of the IP address."""
-        # ip_version = context.dicts[3]["object"].ip_version
+        # Get the IP address from the context.
         ip_address = get_obj_from_context(context)
+
+        # Use ARecordModelTable for IPv4 and AAAARecordModelTable for IPv6.
         if ip_address.ip_version == 4:
             self.table_class = ARecordModelTable
         elif ip_address.ip_version == 6:
             self.table_class = AAAARecordModelTable
+
+        return super().get_extra_context(context)
+
+
+class ReverseDNSRecordsTablePanel(ObjectsTablePanel):
+    """Add PTR DNS Records to the right side of the IP Address page."""
+
+    def get_extra_context(self, context):
+        """Set the table class based on the IP version of the IP address."""
+        user = context.get("request").user
+        if user.has_perm("nautobot_dns_models.view_ptrrecordmodel"):
+            # Calculate the `ptrdname`` based on the IP address.
+            ip_address = get_obj_from_context(context)
+            ptrdname = ipaddress_address(ip_address.host, "reverse_pointer")
+
+            # Construct the table with the filtered PTR records.
+            queryset = PTRRecordModel.objects.filter(ptrdname=ptrdname)
+            ptrdtable = PTRRecordModelTable(queryset)
+
+            # Inject the table into the context.
+            context["ptrdtable"] = ptrdtable
+            self.context_table_key = "ptrdtable"
+
         return super().get_extra_context(context)
 
 
@@ -33,30 +62,18 @@ class IPAddressDNSRecords(TemplateExtension):  # pylint: disable=abstract-method
         ForwardDNSRecordsTablePanel(
             weight=100,
             section=SectionChoices.RIGHT_HALF,
-            # table_class is dynamically set in ForwardDNSRecordsTablePanel.get_extra_context
+            table_class=ARecordModelTable,
             table_filter="address",
             include_columns=["name", "zone", "ttl", "actions"],
-        )
+        ),
+        ReverseDNSRecordsTablePanel(
+            weight=100,
+            section=SectionChoices.RIGHT_HALF,
+            table_class=PTRRecordModelTable,
+            table_filter="ptrdname",
+            include_columns=["name", "zone", "ttl", "actions"],
+        ),
     ]
 
 
-# TODO: Discuss whether PTR Records must have an FK to IP Address (like A/AAAA).
-# If yes, change this to use the UI Component Framework (add to the above class).
-class IPAddressPTRRecords(TemplateExtension):  # pylint: disable=abstract-method
-    """Add DNS PTR Records to the right side of the IP Address page."""
-
-    model = "ipam.ipaddress"
-
-    def right_page(self):
-        """Add content to the right side of the IP Address page."""
-        ptrdname = ipaddress_address(self.context["object"].host, "reverse_pointer")
-        return self.render(
-            "nautobot_dns_models/inc/ipaddress_ptr_records.html",
-            extra_context={
-                "ptr_records": PTRRecordModel.objects.filter(ptrdname=ptrdname),
-                "ptrdname": ptrdname,
-            },
-        )
-
-
-template_extensions = [IPAddressDNSRecords, IPAddressPTRRecords]
+template_extensions = [IPAddressDNSRecords]
