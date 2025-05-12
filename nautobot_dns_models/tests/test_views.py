1fa3f789351a1671df964c43d14b6ac0eb55d083
Gary Snider <75227981+gsnider2195@users.noreply.github.com>
2025-05-12 13:14:30 -0700

    revert changes to test_views.py

--- nautobot_dns_models/tests/test_views.py
+++ nautobot_dns_models/tests/test_views.py
@@ -25,16 +25,6 @@ class DnsZoneModelViewTest(ViewTestCases.PrimaryObjectViewTestCase):
     """Test the DnsZoneModel views."""
 
     model = DNSZoneModel
-    bulk_edit_data = {"description": "Bulk edit views"}
-    form_data = {
-        "name": "Test 1",
-        "description": "Initial model",
-    }
-
-    update_data = {
-        "name": "Test 2",
-        "description": "Updated model",
-    }
 
     @classmethod
     def setUpTestData(cls):
