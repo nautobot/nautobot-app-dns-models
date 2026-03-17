bdb9d690f7f9f930141cd1924681bb4f64363db5
Eric Fetty <eric.fetty@standard.com>
2026-03-17 10:50:27 -0500

    Fix Ruff error Add Registration Navigation

--- nautobot_dns_models/navigation.py
+++ nautobot_dns_models/navigation.py
@@ -1,7 +1,6 @@
 """Menu items to the Nautobot navigation menu."""
 
 from nautobot.apps.ui import (
-    NavigationIconChoices,
     NavigationWeightChoices,
     NavMenuGroup,
     NavMenuItem,
@@ -29,6 +28,24 @@ menu_items = (
                         name="DNS Views",
                         weight=200,
                         permissions=["nautobot_dns_models.view_dnsview"],
+                    ),
+                ),
+            ),
+            NavMenuGroup(
+                name="Registration",
+                weight=400,
+                items=(
+                    NavMenuItem(
+                        link="plugins:nautobot_dns_models:dnsregistrar_list",
+                        name="DNS Registrars",
+                        weight=300,
+                        permissions=["nautobot_dns_models.view_dnsregistrar"],
+                    ),
+                    NavMenuItem(
+                        link="plugins:nautobot_dns_models:dnsregistration_list",
+                        name="DNS Registrations",
+                        weight=400,
+                        permissions=["nautobot_dns_models.view_dnsregistration"],
                     ),
                 ),
             ),
