9843a4787620d9146d98f2c152c8d5f79f209abb
Noah Robin <noahr@nvidia.com>
2026-08-03 12:43:16 -0400

    chore: renumber DNSZone enabled migration after rebase

--- nautobot_dns_models/migrations/0010_dnszone_enabled.py
+++ nautobot_dns_models/migrations/0010_dnszone_enabled.py
@@ -11,7 +11,7 @@ def set_default_enabled(apps, schema_editor):
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("nautobot_dns_models", "0008_dnszone_auto_create_ptr"),
+        ("nautobot_dns_models", "0009_alter_aaaarecord__ttl_alter_arecord__ttl_and_more"),
     ]
 
     operations = [
